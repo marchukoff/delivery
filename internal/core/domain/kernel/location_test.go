4c0042310918fc8ee0bda41f6e1a8d803e9e61f3
Eugene Marchukov <mareugen@yandex.ru>
2025-06-06 14:24:41 +0300

    test: add explicit distance cannot be less than 0

--- internal/core/domain/kernel/location_test.go
+++ internal/core/domain/kernel/location_test.go
@@ -113,10 +113,15 @@ func TestLocation_DistanceTo(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := tt.src.DistanceTo(tt.dst)
+			if got < 0 {
+				t.Errorf("Location.DistanceTo() = %v, it should not be less than 0", got)
+			}
+
 			if (err != nil) && !errors.Is(err, tt.wantErr) {
 				t.Errorf("Location.DistanceTo() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
+
 			if got != tt.want {
 				t.Errorf("Location.DistanceTo() = %v, want %v", got, tt.want)
 			}
