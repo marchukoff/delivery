f53292813262e777ccbbdb723e69cfadef6fdee5
Eugene Marchukov <mareugen@yandex.ru>
2025-06-20 10:44:28 +0300

    feat: implement order dispatcher

--- cmd/composition_root.go
+++ cmd/composition_root.go
@@ -1,9 +1,14 @@
 package cmd
 
-type CompositionRoot struct {
-}
+import "delivery/internal/core/domain/services"
+
+type CompositionRoot struct{}
 
 func NewCompositionRoot(_ Config) CompositionRoot {
 	app := CompositionRoot{}
 	return app
+}
+
+func (c *CompositionRoot) NewOrderDispatcherService() services.OrderDispatcher {
+	return services.NewOrderDispatcher()
 }
